1f0bf4ce4089c76b9c220039ee08bbcf1e3e8b72
datamadic <xavier@openfin.co>
2015-02-24 21:38:28 -0500

    one take jake

---
+++ build/.module-cache/07a89f1e3401092d8815ffe7b7d393b3cb0b7a38.js
@@ -0,0 +1,13 @@
+React.render(
+  React.createElement("h1", null, "Hello, world!"),
+  document.getElementById('example')
+);
+
+var hadit = document.getElementById('hadit');
+
+if (fin) {
+	hadit.innerHTML = 'YES!!!';
+}
+else {
+	hadit.innerHTML = 'WTF MAN!!!!!'
+}
---
+++ src/my.js
@@ -0,0 +1,15 @@
+React.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('example')
+);
+
+var hadit = document.getElementById('hadit');
+
+if (fin) {
+	hadit.innerHTML = 'YES!!!';
+}
+else {
+	hadit.innerHTML = 'WTF MAN!!!!!'
+}
+
+console.log(fin);
